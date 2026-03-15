6aa6ac713b996396647096e48e18ed96643850cd
dheerajsaketh <dheerajsaketh961@gmail.com>
2026-03-16 04:18:26 +0530

    Added portfolio components

---
+++ dheeraj-portfolio/app/components/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "../components/Hero";
+export default function Home() {
+  return (
+    <div className="space-y-32">
+      <Hero />
+
+      {/* Skills */}
+      {/* Projects */}
+      {/* Experience */}
+      {/* Education */}
+      {/* Contact */}
+    </div>
+  );
+}
